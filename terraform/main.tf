233c3a815794bee14256b5f87dfabe3492bb633b
OctavioPalacios <octaviopalacios@OctavioPalacios-MacBook-Pro.local>
2021-11-18 09:35:03 -0600

    test1

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,4 +24,4 @@ resource "azurerm_virtual_network" "main" {
   address_space       = ["10.1.0.0/16"]
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-}
+} 
