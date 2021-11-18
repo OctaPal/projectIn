de683ec96663983cd8cb0b86bb05e00dcfa93a83
OctavioPalacios <octaviopalacios@OctavioPalacios-MacBook-Pro.local>
2021-11-18 09:30:40 -0600

    test1

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,41 +1,27 @@
-data "azurerm_resource_group" "my-rg" {
-  name = "tfstaterg"
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "octavio-example"
+  location = "West Europe"
 }
 
 
-resource "azurerm_virtual_network" "network" {
-  name                = "${var.prefix}-vn-act3"
+#------- VIRTUAL NETWORK -----------#
+resource "azurerm_virtual_network" "main" {
+  name                = "octapal-network"
   address_space       = ["10.1.0.0/16"]
-  location            = data.azurerm_resource_group.my-rg.location
-  resource_group_name = data.azurerm_resource_group.my-rg.name
-}
-
-
-resource "azurerm_subnet" "example" {
-  name                 = "subnet-act3"
-  resource_group_name  = data.azurerm_resource_group.my-rg.name
-  virtual_network_name = azurerm_virtual_network.network.name
-  address_prefixes     = ["10.1.2.0/24"]
-}
-
-resource "azurerm_network_security_group" "example" {
-  name                = "nsg-ac2"
-  location            = data.azurerm_resource_group.my-rg.location
-  resource_group_name = data.azurerm_resource_group.my-rg.name
-
-  security_rule {
-    name                       = "testac2"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  tags = {
-    environment = "dev"
-  }
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 }
